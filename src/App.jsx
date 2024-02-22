79c41ccff3fc8310ea4bbfd24e7524a24c6a094b
Shubh <shubh2268@gmail.com>
2024-02-22 17:16:03 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center underline">
-        Hello Shubh!
-      </h1>
+      <Navbar />
     </div>
   )
 }
