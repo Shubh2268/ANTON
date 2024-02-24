0480c6ca3e86f74212394fb90e7f231ddb503559
Shubh <shubh2268@gmail.com>
2024-02-24 20:08:19 +0530

    --

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,18 +20,18 @@ const Home = ({ isMenuShown }) => {
     }
 
     return (
-        <div name='home' className='flex items-end justify-center w-full h-screen text-center'>
+        <div name='home' className='flex items-center md:items-end justify-center w-full h-screen'>
 
             <video ref={videoRef} src={bgVideo} autoPlay loop muted className='object-cover h-full w-full absolute -z-10' />
 
             <div className={`p-8 flex flex-col items-center justify-center duration-500 w-full ${isVideoPlaying ? '' : 'bg-black/80'} ${isMenuShown ? 'opacity-20' : 'opacity-100'}`}>
 
-                <h1 className='text-gray-200 tracking-wide text-4xl lg:text-5xl'>Web 3.0</h1>
-                <h1 className='text-gray-200 tracking-wide text-4xl lg:text-5xl capitalize mb-12'>the <span className='text-thBlue font-bold'>future</span> is here</h1>
+                <h1 className='text-gray-200 tracking-wide text-2xl md:text-3xl lg:text-5xl'>Web 3.0</h1>
+                <h1 className='text-gray-200 tracking-wide text-2xl md:text-3xl lg:text-5xl capitalize mb-12'>the <span className='text-thBlue font-bold'>future</span> is here</h1>
 
                 <div className='flex flex-row justify-center items-center'>
                     <GradientBtn className='text-xs lg:text-base capitalize' title='get anton' />
-                    <GradientBtn className='capitalize mx-12' title='products' />
+                    <GradientBtn className='text-xs lg:text-base capitalize mx-3' title='products' />
 
                     {
                         isVideoPlaying ? <FaPauseCircle size={30} onClick={handleVideoPause} className='cursor-pointer hover:scale-110 ease-in duration-200 text-thBlue' />
